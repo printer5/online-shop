cb592ac073d2719aae84b9dd0173d5c49a9eea85
A1exSid <alexsiboleg@gmail.com>
2026-03-20 18:52:35 +0700

    Create database.py

---
+++ database.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect("shop.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS product (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price REAL NOT NULL,
+            description TEXT DEFAULT "",
+            image TEXT DEFAULT ""         
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+def add_product(name, price, description="", image=""):
+    conn = sqlite3.connect("shop.db")
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO products 
+        (name, price, description, image)
+        VALUES (?, ?, ?, ?)
+    ''', (name, price, description, image))
+    conn.commit()
+    conn.close()
+
+def get_product():
+    conn = sqlite3.connect("shop.db")
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM products')
+    products = cursor.fetchall()
+
+    conn.close()
+    return products
+
+if __name__ == "__main__":
+    create_db()
+    add_product("test product", 9.89, "opisanie")
+    add_product("test product2", 19.29, "opisanie2")
+    print(get_product())
+
+
